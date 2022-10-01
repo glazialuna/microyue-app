faaf1797a8a55f3469e7acd58407e4f480adb286
glazialuna <glaziawy@gmail.com>
2022-09-30 22:19:22 -0500

    initial page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+
+
+
+
 function App() {
+  const today = new Date();
+  const day = today.toLocaleString([], {weekday: 'long'});
+  const date = today.toLocaleDateString([], {dateStyle: 'long'})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sample React code</h1>
+      
+      <p>Today is {day}, {date}.</p>
     </div>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
