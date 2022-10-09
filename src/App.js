25c7dcbcc3f9f3eea961cbacf6da52010e144df7
glazialuna <glaziawy@gmail.com>
2022-10-08 19:46:07 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
+
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -33,12 +36,28 @@ const schedule = {
   }
 };
 
+
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://dummyjson.com/users');
+
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
+
+  return data.users.map(user => <div key={user.id}>{user.firstName} {user.lastName}</div>);
+}
+
+const queryClient = new QueryClient();
+
 function App(){
   return (
-    <div>
-      <Banner text={schedule.title} />
-      <CourseList courses={schedule.courses} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        {/* <Banner text={schedule.title} />
+        <CourseList courses={schedule.courses} /> */}
+        <Main/>
+      </div>
+    </QueryClientProvider>
   );
 } 
 
