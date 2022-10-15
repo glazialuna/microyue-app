b6ae7a1f4f6235d4fe5c4f06c16462217bbdd0d6
glazialuna <glaziawy@gmail.com>
2022-10-14 22:42:15 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -6,50 +6,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
 import datas from './utilities/cs-courses.json';
 import TermPage from './components/TermPage';
-import SelectedPage from './components/CourseSelection';
 
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
 
 
 // loading data from php
 const Main = () => {
-  // return (
-  //   <div>
-  //     <Banner text={datas.title} />
-  //     <CourseList courses={datas.courses} />
-  //   </div>
-  // );
-
-
   const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading user data...</h1>;
@@ -57,13 +18,14 @@ const Main = () => {
 
   return (
     <div>
-      <Banner text={data.title} />
-      <CourseList courses={data.courses} />
+      {/* datas from local, data from php  */}
+      <TermPage courses={data.courses}></TermPage>
     </div>
   );
 
 
 }
+
 
 const queryClient = new QueryClient();
 
@@ -71,8 +33,7 @@ function App(){
   return (
     <QueryClientProvider client={queryClient}>
       <div>
-        {/* <Main></Main> */}
-        <TermPage courses={datas.courses}></TermPage>
+        <Main></Main>
       </div>
     </QueryClientProvider>
   );
